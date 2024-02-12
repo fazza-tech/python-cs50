c5a355a0d32e87712a902352b70ce48ebe58c2cb
fazza-tech <ebranfazza@gmail.com>
2024-02-12 18:11:35 +0530

    new commits

---
+++ loops.py
@@ -0,0 +1,21 @@
+sum = 0
+for i in range(10):
+    sum = sum + i
+    print(sum)
+
+
+
+for x in range(100):
+    print(x)
+
+
+names = ['Fasal','Jyobish','Sahul','Sudhev']
+
+for name in names:
+    print(name)
+
+
+myName = "Fasal"
+
+for character in myName:
+    print(character)
--- set.py
+++ set.py
@@ -10,3 +10,5 @@ s.add(120)
 
 s.remove(4)
 print(s)
+
+print(f"set has {len(s)} elements")
